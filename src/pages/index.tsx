4796ab28889a960bd8b04c9d8ec69b7a3fac7fa9
Bassem Dimassi <dimassibassem99@gmail.com>
2022-09-07 23:46:51 +0100

    :art: chat Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,128 @@
-import type {NextPage} from 'next'
+import io, {Socket} from "socket.io-client";
+import React, {useState, useEffect, useId} from "react";
+import {NextPage} from "next";
 
-const Home: NextPage = () => (
-    <>
-        Hello World
-    </>
-)
+let socket: Socket;
 
-export default Home
+type Message = {
+    author: string;
+    message: string;
+};
+
+const Home: NextPage = () => {
+    const [username, setUsername] = useState("");
+    const [chosenUsername, setChosenUsername] = useState("");
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState<Array<Message>>([]);
+
+    const socketInitializer = async () => {
+        // We just call it because we don't need anything else out of it
+        await fetch("/api/socketio");
+
+        socket = io();
+
+        socket.on("newIncomingMessage", (msg) => {
+            setMessages((currentMsg) => [
+                ...currentMsg,
+                {author: msg.author, message: msg.message},
+            ]);
+            // console.log(messages);
+        });
+    };
+
+    useEffect(() => {
+        socketInitializer().catch((err) => console.log(err));
+    }, []);
+
+    const sendMessage = async () => {
+        socket.emit("createdMessage", {author: chosenUsername, message});
+        setMessages((currentMsg) => [
+            ...currentMsg,
+            {author: chosenUsername, message},
+        ]);
+        setMessage("");
+    };
+
+    const handleKeypress = (e: React.KeyboardEvent<HTMLElement>) => {
+        // it triggers by pressing the enter key
+        if (e.keyCode === 13) {
+            if (message) {
+                sendMessage();
+            }
+        }
+    };
+
+    const id = useId()
+
+    return (
+        <div className="flex items-center p-4 mx-auto min-h-screen justify-center bg-purple-500">
+            <main className="gap-4 flex flex-col items-center justify-center w-full h-full">
+                {!chosenUsername ? (
+                    <>
+                        <h3 className="font-bold text-white text-xl">
+                            How people should call you?
+                        </h3>
+                        <input
+                            type="text"
+                            placeholder="Identity..."
+                            value={username}
+                            className="p-3 rounded-md outline-none"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <button
+                            type="button"
+                            onClick={() => {
+                                setChosenUsername(username);
+                            }}
+                            className="bg-white rounded-md px-4 py-2 text-xl"
+                        >
+                            Go!
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <p className="font-bold text-white text-xl">
+                            Your username: {username}
+                        </p>
+                        <div className="flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md ">
+                            <div className="h-full last:border-b-0 overflow-y-scroll">
+                                {messages.map((msg, i) => (
+                                    <div
+                                        className="w-full py-1 px-2 border-b border-gray-200"
+                                        key={`${id + i}`}
+                                    >
+                                        {msg.author} : {msg.message}
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="border-t border-gray-300 w-full flex rounded-bl-md">
+                                <input
+                                    type="text"
+                                    placeholder="New message..."
+                                    value={message}
+                                    className="outline-none py-2 px-2 rounded-bl-md flex-1"
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    onKeyUp={handleKeypress}
+                                />
+                                <div
+                                    className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all">
+                                    <button
+                                        type="button"
+                                        className="group-hover:text-white px-3 h-full"
+                                        onClick={() => {
+                                            sendMessage();
+                                        }}
+                                    >
+                                        Send
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </main>
+        </div>
+    );
+}
+
+export default Home;
