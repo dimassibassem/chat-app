7ee70c2104f89d7249a2c57357436764fbed4499
Bassem Dimassi <dimassibassem99@gmail.com>
2022-09-09 22:26:31 +0100

    Auth with Google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {SessionProvider} from "next-auth/react";
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+// @ts-ignore
+const MyApp = ({Component, pageProps: {session, pageProps}}: AppProps) =>
+    <SessionProvider session={session}>
+        <Component {...pageProps} />
+    </SessionProvider>
 
 export default MyApp
