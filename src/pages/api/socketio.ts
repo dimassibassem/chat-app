8c4c71fe4624c7cadbbe791141448ca13505d47b
Bassem Dimassi <dimassibassem99@gmail.com>
2022-09-11 23:32:58 +0100

    Code update

--- src/pages/api/socketio.ts
+++ src/pages/api/socketio.ts
@@ -2,6 +2,10 @@ import {Server} from 'socket.io'
 import {NextApiRequest, NextApiResponse} from "next";
 import messageHandler, {ServerToClientEvents} from '@/utils/messageHandler';
 
+type Data = {
+    author?: string,
+}
+
 const ioHandler = (req: NextApiRequest,
                    res: NextApiResponse) => {
     // It means that socket server was already initialised
@@ -20,13 +24,11 @@ const ioHandler = (req: NextApiRequest,
     // Define actions inside
     io.on("connection", (socket: ServerToClientEvents) => {
         messageHandler(io, socket);
-        socket.on("typing", (data: object) => {
-            // @ts-ignore
-            socket.broadcast.emit("typing", data.author);
+        socket.on("typing", (data: Data) => {
+            socket.broadcast.emit("typing", data.author || "Anonymous");
         })
-        socket.on("stopTyping", (data: object) => {
-            // @ts-ignore
-            socket.broadcast.emit("stopTyping", data.author);
+        socket.on("stopTyping", (data: Data) => {
+            socket.broadcast.emit("stopTyping", data.author || "Anonymous");
         })
     });
 
