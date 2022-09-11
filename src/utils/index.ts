1299bb4c742a9eaf19efea1098e2b59312a2eee3
Bassem Dimassi <dimassibassem99@gmail.com>
2022-09-12 00:43:07 +0100

    Code Management

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,15 @@
 import {KeyboardEvent} from "react";
 import {Socket} from "socket.io-client";
+import {Session} from "next-auth";
+import axios from "axios";
 
 const timer = (author: string, socket: Socket) => setTimeout(() => {
     socket.emit("stopTyping", {author});
 }, 2000)
 
-export const handleKeypress = (e: KeyboardEvent<HTMLElement>, author: string, socket: Socket, message: any, sendMessage: () => void) => {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
+export const handleKeypress = (e: KeyboardEvent<HTMLElement>, author: string, socket: Socket, message: any, sendMessage: any) => {
     let timerId = window.setTimeout(() => {
     }, 0);
-    // eslint-disable-next-line no-plusplus
     while (timerId--) {
         window.clearTimeout(timerId); // will do nothing if no timeout with id is present
     }
@@ -24,3 +24,18 @@ export const handleKeypress = (e: KeyboardEvent<HTMLElement>, author: string, so
     timer(author, socket)
     return () => clearTimeout(timer(author, socket));
 };
+
+export const addUserIfNotExist = async (activeSession: Session) => {
+    await axios.post("/api/user", {
+        name: activeSession.user?.name,
+        email: activeSession.user?.email,
+        image: activeSession.user?.image
+    })
+}
+
+export const getUser = async (session: Session, setConnectedUser: (arg0: any) => void) => {
+    const res = await axios.post("/api/findUser", {
+        email: session?.user?.email
+    });
+    setConnectedUser(res.data);
+}
