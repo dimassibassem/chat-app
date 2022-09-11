b8d31db1691077c41596f074d6fe3d3676c1cf04
Bassem Dimassi <dimassibassem99@gmail.com>
2022-09-11 23:33:10 +0100

    Code update

--- src/pages/api/usersWithMessages.ts
+++ src/pages/api/usersWithMessages.ts
@@ -5,6 +5,17 @@ import prisma from '@/utils/prismaClient';
 const usersWithMessages = async (req: NextApiRequest,
                                  res: NextApiResponse) => {
 
+    type UserMessage = {
+        id: number,
+        content: string,
+        senderId?: number,
+        receiverId?: number,
+        createdAt: Date,
+        updatedAt: Date
+        status: string,
+    }
+
+
     const data = await prisma.user.findMany({
         where: {
             email: {
@@ -18,6 +29,8 @@ const usersWithMessages = async (req: NextApiRequest,
                     content: true,
                     createdAt: true,
                     updatedAt: true,
+                    receiverId : true,
+                    senderId: true,
                 }
             },
             sentMessages: {
@@ -26,12 +39,39 @@ const usersWithMessages = async (req: NextApiRequest,
                     content: true,
                     createdAt: true,
                     updatedAt: true,
+                    senderId: true,
+                    receiverId: true,
                 }
             }
         }
     })
 
-    res.status(200).json(data)
+    const result = data.map((user: any) => {
+            const messages: Array<UserMessage> = []
+            user.receivedMessages.map((message: UserMessage) => messages.push({
+                id: message.id,
+                content: message.content,
+                senderId: message.senderId,
+                createdAt: message.createdAt,
+                updatedAt: message.updatedAt,
+                status: 'RECEIVED'
+            }))
+
+            user.sentMessages.map((message: UserMessage) => messages.push({
+                id: message.id,
+                content: message.content,
+                receiverId: message.receiverId,
+                createdAt: message.createdAt,
+                updatedAt: message.updatedAt,
+                status: 'SENT'
+            }))
+        const {sentMessages, receivedMessages, ...rest} = user
+        messages.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
+        return {...rest, messages}
+        }
+    )
+
+    return res.status(200).json(result)
 }
 
 export default usersWithMessages
