290e5c64a792c600128b8f534250450343ab2264
Bassem Dimassi <dimassibassem99@gmail.com>
2022-09-12 13:23:31 +0100

    Add message To Database

--- src/utils/messageHandler.ts
+++ src/utils/messageHandler.ts
@@ -1,8 +1,18 @@
 import {Server} from 'socket.io'
-import {ServerToClientEvents} from '@/utils/types';
+import {Message, ServerToClientEvents} from '@/utils/types';
+import prisma from "@/utils/prismaClient";
 
 export default (io: Server, socket: ServerToClientEvents) => {
-    socket.on("createdMessage", (msg: object) => {
+    socket.on("createdMessage", async (msg: Message) => {
+        await prisma.message.create({
+            data: {
+        // @ts-ignore
+                content: msg.content,
+                receiverId: msg.receiverId,
+                senderId: msg.senderId,
+                createdAt: msg.createdAt
+            }
+        })
         socket.broadcast.emit("newIncomingMessage", msg);
     });
 };
