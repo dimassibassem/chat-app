09b44798301e35d82bcd27660f791b814acebbf0
Bassem Dimassi <dimassibassem99@gmail.com>
2022-09-13 23:47:45 +0100

    Type Correction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,7 +78,6 @@ const Home: NextPage = () => {
 
     ref.current?.scrollIntoView({behavior: "smooth"})
 
-
     console.log(someoneIsTyping);
     return (
         <div className="flex items-center p-4 mx-auto min-h-screen justify-center bg-purple-500">
